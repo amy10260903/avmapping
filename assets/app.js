8f8164a886f22eb95dbf3b99ca708ccbf64ce8d1
amy10260903 <amy10260903@gapp.nthu.edu.tw>
2019-11-07 17:31:13 +0800

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -28,7 +28,7 @@ cameraTrigger.onclick = function() {
     dataURL = cameraOutput.src;
     $.ajax({
         type: "POST",
-        url: "https://140.114.78.91:8802/cd",
+        url: "http://140.114.78.91:8802/cd",
         data: { 
             imgBase64: dataURL
         },
